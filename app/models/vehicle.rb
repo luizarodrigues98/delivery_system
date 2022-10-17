3d091b1fa001484822f2b3b3c4e00ffd8cbc342f
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-10-17 14:17:20 -0300

    ajustes nos formulários

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,6 +2,7 @@ class Vehicle < ApplicationRecord
   belongs_to :transport_type
   has_one :service_order
   validates :plate, presence: true
+  validates :plate, uniqueness: true
   validates :brand, presence: true
   validates :model, presence: true
   validates :fabrication_year, presence: true
